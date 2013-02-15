2649970c44f8b78d4bb2172dd8c94f0455993876
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-02-15 19:00:24 +0100

    Removed sortable and sorting flag as it does not make sense for tags.

--- src/system/modules/metamodelsattribute_translatedtags/dca/tl_metamodel_dcasetting.php
+++ src/system/modules/metamodelsattribute_translatedtags/dca/tl_metamodel_dcasetting.php
@@ -4,16 +4,14 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
 array
 (
 	'presentation' => array(
-        'tl_class',    
-    ),
-    'functions'  => array(
-        'mandatory'
-    ),
-	'overview' => array(		
+		'tl_class',
+	),
+	'functions'  => array(
+		'mandatory'
+	),
+	'overview' => array(
 		'filterable',
 		'searchable',
-		'sortable',
-		'flag'
 	)
 );
 
