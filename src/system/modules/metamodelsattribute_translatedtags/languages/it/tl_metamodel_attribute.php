49c4587761e134258d8f5aa176bff316a457df9d
Mini Model <minimodel@metamodel.me>
2013-06-27 20:14:48 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_translatedtags/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_translatedtags/languages/it/tl_metamodel_attribute.php
@@ -8,9 +8,13 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-04T02:35:30+02:00
+ * last-updated: 2013-06-23T04:14:42+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['0'] = 'Colonna di linguaggio';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['0'] = 'Colonna di ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['0']           = 'Colonna di linguaggio';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['1']           = 'Specificare quale colonna contiene il codice della lingua in formato ISO 639-1';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['0']           = 'Colonna di ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['1']           = 'Specificare quale colonna della tabella non tradotta deve essere utilizzata per l\'ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['0']             = 'Tabella non tradotta per l\'ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['1']             = 'Specificare la tabella che mostra la colonna di ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedtags'] = 'Tags tradotto';
