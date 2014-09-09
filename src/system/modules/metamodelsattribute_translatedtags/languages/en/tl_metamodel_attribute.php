0b8e6055b7d1953b9d9e4d903167e337bfb10c81
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-09-09 05:43:10 +0200

    Fix spacing in language file.

--- src/system/modules/metamodelsattribute_translatedtags/languages/en/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_translatedtags/languages/en/tl_metamodel_attribute.php
@@ -20,6 +20,12 @@
  * Fields
  */
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedtags'] = 'Translated tags';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']                = array('Language column', 'Please specify which column holds the language code in ISO 639-1');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']                  = array('Untranslated table for sorting', 'Please specify the table that provides the sorting column.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']                = array('Sorting column', 'Please specify which column of the untranslated table shall be used for sorting.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn'][0]             = 'Language column';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn'][1]             =
+    'Please specify which column holds the language code in ISO 639-1';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable'][0]               = 'Untranslated table for sorting';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable'][1]               =
+    'Please specify the table that provides the sorting column.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting'][0]             = 'Sorting column';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting'][1]             =
+    'Please specify which column of the untranslated table shall be used for sorting.';
