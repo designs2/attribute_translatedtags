b5888c7069498e6cccff8c2921a23325d2f55ad3
Mini Model <minimodel@metamodel.me>
2015-09-18 10:31:53 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:01:59+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['0']           = 'Язык колонки';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_langcolumn']['1']           = 'Укажите, какая колонка содержит код языка по ISO 639-1';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['0']           = 'Сортировка колонок';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srcsorting']['1']           = 'Укажите, какая колонка непереведеннной таблицы будет использована для сортировки.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['0']             = 'Непереведенная таблица для сортировки';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_srctable']['1']             = 'Укажите таблицу содержащую колонку сортировки.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedtags'] = 'Переводить теги';
+
